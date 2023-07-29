249efc95d3dd7bbfe5279d12f5bc165b48370f9e
Justin Lee <justinslee201100@yahoo.com>
2023-07-29 15:32:56 -0600

    Add puts and reassigned string values

--- variables.rb
+++ variables.rb
@@ -7,5 +7,14 @@ prefered_number_of_breakfasts = 2
 shoe_size = 10.5
 pi = 3.14
 
-likes_dogs = true
-likes_asparagus = false 
+likes_dogs = false
+likes_asparagus = true
+
+name = "Justin" if likes_dogs = true
+name = "Francisco" if likes_asparagus = true
+email = "francisco@yahoo.com" if likes_dogs = false
+
+puts name
+puts email
+puts 5 + 3
+puts "Hello" + name + "!"
